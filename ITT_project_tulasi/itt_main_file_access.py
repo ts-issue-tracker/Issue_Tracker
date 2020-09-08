32ac65b18e5b77d08afbf4f55b60aa16aac33ced
Tulasi Jana <jtdurga015@gmail.com>
2020-09-09 08:37:31 +1400

    Adding itt_main_file_access.py for supported funtionality code used in itt_display_charts.py

---
+++ ITT_project_tulasi/itt_main_file_access.py
@@ -0,0 +1,46 @@
+import xlrd
+# Give the location of the file
+file_location = "main.xlsx"
+
+wb = xlrd.open_workbook(file_location)
+sheet = wb.sheet_by_index(0)
+row = 0
+col = 0
+result_list = []
+
+def return_col_of_specified_col_name(col_name):
+    for i in range(sheet.nrows):
+        for j in range(sheet.ncols):
+            if sheet.cell_value(i,j)==col_name:
+                return j
+    return -1
+
+def return_list(counting_list,label_list,col):
+    for i, j in zip(label_list, range(0,len(label_list) )):
+        for k in range(sheet.nrows):
+            if sheet.cell_value(k,col)==i:
+                counting_list[j]+=1
+    return  counting_list
+def list_to_display_on_pie(col_name):
+    col = return_col_of_specified_col_name(col_name)
+    print(col)
+    label_list = []
+    label_list.extend(return_label_list(col_name))
+    list2 = []
+    for i in range(0, len(label_list)):
+        list2.append(0)
+    return return_list(list2, label_list, col)
+
+def return_label_list(col_name):
+    state_list = ["Open", "Analysis", "In Progress", "Reopen", "Closed"]
+    domain_list = ["Audio", "Video", "Camera"]
+    issue_list = ["Bug", "Internal", "Blacklisting"]
+    assignee_list = ["Tulasi", "Santhoshi", "Pavani", "Suma", "Suresh"]
+
+    get_label_list_of_specified_col_name = {
+        "State": state_list,
+        "Domain": domain_list,
+        "IssueType": issue_list,
+        "Assignee": assignee_list
+    }
+    return get_label_list_of_specified_col_name.get(col_name)
