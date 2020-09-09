4a656d3c6dd9b2e89406345baece4ad69eb47012
Tulasi Jana <jtdurga015@gmail.com>
2020-09-09 19:36:57 +1400

    Added funtionality code for assignee related filters used in pie chart

--- ITT_project_tulasi/itt_main_file_access.py
+++ ITT_project_tulasi/itt_main_file_access.py
@@ -30,6 +30,26 @@ def list_to_display_on_pie(col_name):
     for i in range(0, len(label_list)):
         list2.append(0)
     return return_list(list2, label_list, col)
+def list_to_display_on_pie_for_assignee(assignee_col,assignee_name,col_name):
+    assignee_filter_list=[]
+    col = return_col_of_specified_col_name(col_name)
+    print(col)
+    for k in range(sheet.nrows):
+        if sheet.cell_value(k, assignee_col) == assignee_name:
+            assignee_filter_list.append(sheet.cell_value(k, col))
+    if len(assignee_filter_list)!=0:
+        list=[]
+        list.extend(return_label_list(col_name))
+        print(list)
+        list2=[]
+        for i in range(0,len(list)):
+            list2.append(0)
+        for i,k in zip(list,range(0,len(list))):
+            for j in assignee_filter_list:
+                if i==j:
+                    list2[k]+=1
+    print(list2)
+    return list2
 
 def return_label_list(col_name):
     state_list = ["Open", "Analysis", "In Progress", "Reopen", "Closed"]
