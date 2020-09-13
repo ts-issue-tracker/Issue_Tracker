b06b0abfb42572af0109143dd9a3abec3c9eed2c
Tulasi Jana <jtdurga015@gmail.com>
2020-09-13 20:30:24 +1400

    Updated mail with sending message info display statistics

--- ITT_project_tulasi/itt_mail_sending.py
+++ ITT_project_tulasi/itt_mail_sending.py
@@ -24,3 +24,24 @@ def sending_registration_mail_to(toaddrs):
     server.send_message(msg)
     server.ehlo()
     server.quit()
+
+def sending_mail_with_selected_statistics_info(toaddrs,msgtxt):
+    fromaddr = "tulasi.jana"
+    # toaddrs="tulasi.jana@thundersoft.com"
+    SMTPServer = "mail.thundersoft.com"
+    port = 465  # 587
+    login = "tulasi.jana@thundersoft.com"
+    password = "tulasi@tsoft1994"
+    msg = EmailMessage()
+    msg.set_content(msgtxt)
+    msg['Subject'] = "Issue Tracking Tool: Selected Statistics Information"
+    msg['From'] = fromaddr
+    msg['To'] = toaddrs
+    server=smtplib.SMTP_SSL(SMTPServer, port)
+    #use smtplib.SMTP() if port is 587
+    #server.starttls() 
+    server.ehlo()
+    server.login(login, password)
+    server.send_message(msg)
+    server.ehlo()
+    server.quit()
